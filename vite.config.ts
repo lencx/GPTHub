31be9ea4e10d448c71b84e92458d23cfb8292d1f
lencx <cxin1314@gmail.com>
2023-11-13 10:51:24 +0800

    chore: path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gpthub',
+  base: '/',
   plugins: [
     tsconfigPaths(),
     react(),
