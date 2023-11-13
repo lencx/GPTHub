df50e81c61c98b08ac1b4b53d95bc76e2b3d943d
lencx <cxin1314@gmail.com>
2023-11-13 09:26:29 +0800

    chore: path

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import BaseLayout from '@/layouts/base';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// eslint-disable-next-line react-refresh/only-export-components
+function App() {
+  useLayoutEffect(() => {
+    if (window.location.pathname === '/') {
+      window.location.href = '/gpthub/';
+    }
+  }, [])
+  return (
     <BrowserRouter basename="/gpthub">
       <BaseLayout />
     </BrowserRouter>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>,
 )
