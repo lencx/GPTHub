31f959835ce699099947efb0c60ac9c99069ed89
lencx <cxin1314@gmail.com>
2023-11-13 10:45:11 +0800

    chore: path

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/gpthub',
   plugins: [
     tsconfigPaths(),
     react(),
     splitVendorChunkPlugin(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      // devOptions: {
-      //   enabled: true,
-      // },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   // devOptions: {
+    //   //   enabled: true,
+    //   // },
+    // }),
   ],
 })
