240cc25620c795fb4de37019746077fd3bc804e2
lencx <cxin1314@gmail.com>
2023-11-13 12:32:34 +0800

    chore: comments

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,10 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 // import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        404: resolve(__dirname, '404.html'),
-      },
-    },
-  },
   plugins: [
     tsconfigPaths(),
     react(),
